a07c7fc3cda014a6758cca3559d060cdbaa06663
Stephen <stephendeanharper@gmail.com>
2018-04-26 09:33:58 -0700

    completed kata

--- highest-scoring-word/solution.js
+++ highest-scoring-word/solution.js
@@ -1,19 +1,19 @@
 function high(x){
-  var points = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var words = x.split(' ');
-  var scores = [];
+  let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let words = x.split(' ');
+  let totalScores = [];
 
   words.forEach(word => {
-    var score = 0;
+    let scoreCounter = 0;
     word.split('').forEach(letter => {
-      score += points.indexOf(letter) + 1;
+      scoreCounter += letters.indexOf(letter) + 1;
     });
-    scores.push(score);
+    totalScores.push(scoreCounter);
   });
 
-  var highest = scores.reduce((a,b) => {
+  let highest = totalScores.reduce((a,b) => {
     return Math.max(a,b);
   });
 
-  return words[scores.indexOf(highest)];
+  return words[totalScores.indexOf(highest)];
 }
